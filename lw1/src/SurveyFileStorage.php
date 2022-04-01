f801e5649847ff492d5846fd5d771f056363d73d
GsG-bOIIZ <danilaalambaev@gmail.com>
2022-04-02 02:49:14 +0300

    lw1 made changes

--- lw1/src/SurveyFileStorage.php
+++ lw1/src/SurveyFileStorage.php
@@ -7,6 +7,7 @@ class SurveyFileStorage
     private const FILE_LAST_NAME = "Last Name";
     private const FILE_EMAIL = "Email";
     private const FILE_AGE = "Age";
+    private const DELIMETER_PARAMETERS = ": ";
 
     private function createFileName(Survey $survey): string
     {
@@ -22,7 +23,7 @@ class SurveyFileStorage
             $arrayData = [];
             foreach ($tempArray as $line)
             {
-                $tempString = explode(': ', $line);
+                $tempString = explode(self::DELIMETER_PARAMETERS, $line);
                 $arrayData[$tempString[0]] = $tempString[1] ?? null;
             }
             return new Survey(
@@ -34,7 +35,7 @@ class SurveyFileStorage
         }  
         else
         {
-            echo "<br>Impossible email (load Survey from file)<br><br>";
+            echo "<br>Impossible email (for load Survey from file)<br><br>";
             return new Survey(null, null, null, null);
         }      
     }
@@ -46,21 +47,27 @@ class SurveyFileStorage
             echo "<br>Impossible email (for save Survey to file)<br><br>";
             return;
         }
+
         $fileName = self::createFileName($survey);
+        $tempEmail = self::FILE_EMAIL . self::DELIMETER_PARAMETERS;
+        $tempFirstName = self::FILE_FIRST_NAME . self::DELIMETER_PARAMETERS;
+        $tempLastName = self::FILE_LAST_NAME . self::DELIMETER_PARAMETERS;        
+        $tempAge = self::FILE_AGE . self::DELIMETER_PARAMETERS;
+
         if (file_exists($fileName))
         {
             $tempArray = file($fileName);
             if ($survey->getFirstName() !== null)
             {
-                $tempArray[1] = "First Name: " . $survey->getFirstName() . "\n";
+                $tempArray[1] = $tempFirstName . $survey->getFirstName() . "\n";
             }
             if ($survey->getLastName() !== null)
             {
-                $tempArray[2] = "Last Name: " . $survey->getLastName() . "\n";
+                $tempArray[2] = $tempLastName . $survey->getLastName() . "\n";
             }
             if ($survey->getAge() !== null)
             {
-                $tempArray[3] = "Age: " . $survey->getAge();
+                $tempArray[3] = $tempAge . $survey->getAge();
             }
             file_put_contents($fileName, $tempArray);
         }
@@ -71,10 +78,10 @@ class SurveyFileStorage
                 mkdir(self::FOLDER_DATA);
             }
             $surveyTxt = fopen($fileName, "w");
-            fwrite($surveyTxt, "Email: " . $survey->getEmail() . "\n");
-            fwrite($surveyTxt, "First Name: " . $survey->getFirstName() . "\n");
-            fwrite($surveyTxt, "Last Name: " . $survey->getLastName() . "\n");            
-            fwrite($surveyTxt, "Age: " . $survey->getAge());
+            fwrite($surveyTxt, $tempEmail . $survey->getEmail() . "\n");
+            fwrite($surveyTxt, $tempFirstName . $survey->getFirstName() . "\n");
+            fwrite($surveyTxt, $tempLastName . $survey->getLastName() . "\n");            
+            fwrite($surveyTxt, $tempAge . $survey->getAge());
             fclose($surveyTxt);
         }
     }   
